7cde414479a0abeb634a7ae29d378ab1b2079a99
KaMeHb-UA <marlock@etlgr.com>
2018-03-19 17:04:36 +0200

    Using external module to check types

--- main.js
+++ main.js
@@ -1,30 +1,11 @@
 const {getPrivate, setPrivate} = require('node-object-private-props'),
-    Promise = require('bluebird');
+    Promise = require('bluebird'),
+    checkType = require('type-checker');
 function genetateANSIColor(color, string, background){
     return `\x1b[${background ? (`48;2;${background.r};${background.g};${background.b}`) : 49};${color ? (`38;2;${color.r};${color.g};${color.b}`) : 39}m${string}\x1b[0m`
 }
 function normalizeTime(tnum){
     return tnum < 10 ? `0${tnum}` : tnum
-}
-function checkType(variable, type, checkProto = true){
-    if (
-        type === variable || // for undefined too
-        (
-            type.name &&
-            type.name.toLowerCase &&
-            type.name.toLowerCase.apply &&
-            ((typeof variable)[0].toUpperCase() + (typeof variable).slice(1)) == type.name
-        ) ||
-        (
-            type.isPrototypeOf &&
-            type.isPrototypeOf.apply &&
-            type.isPrototypeOf(variable)
-        ) || (
-            (type.prototype || type.__proto__.constructor) &&
-            checkProto &&
-            checkType(variable, type.prototype || type.__proto__.constructor, false)
-        )
-    ) return true; else return false;
 }
 class LeNode{
     constructor(settings){
@@ -53,7 +34,9 @@ class LeNode{
     start(port){
         var settings = getPrivate(this, 'settings');
         settings.router.forEach(site => {
-            if(typeof site.domain != 'object' || RegExp.$1 || typeof site.domain != 'string')
+            if (!checkType(site.domain, RegExp) || !checkType(site.folder, String)){
+                // not
+            }
         });
     }
 }
