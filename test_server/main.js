81cde486a842afab128579c42fbc22ef4889a586
KaMeHb-UA <marlock@etlgr.com>
2018-06-08 03:41:41 +0300

    Intermediate

--- test_server/main.js
+++
@@ -1,9 +0,0 @@
-const {LeNode} = require(__dirname + '/..');
-new LeNode({
-    router: [
-        {
-            domain: /localhost/,
-            folder: '/home/superuser/Документы/GitHub/Lenode-3/test_server/test_site'
-        },
-    ],
-}).start(8080);
